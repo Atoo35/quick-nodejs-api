573ae8612273ecb842d24235155d31d4f9ed6f3b
Atharva Deshpande <adrooney322@gmail.com>
2022-10-16 19:57:52 +0000

    testing

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const app = express()
-const port = 3005
+const port = 3000
 
 app.get('/', (req, res) => {
   res.json({ message: "HelloWorld!", secret: process.env.TESTING })
 })
 
 app.listen(port, () => {
+  // const { Pool } = require("pg")
+
+  // const db = new Pool()
+
+  // db.query(`CREATE TABLE IF NOT EXISTS testing(id SERIAL PRIMARY KEY);`)
   console.log(`Example app listening on port ${port}`)
 })
