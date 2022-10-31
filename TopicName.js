4e0945d88f2fe51c59b1df2e30dcdd8715a5df27
Atharva Deshpande <adrooney322@gmail.com>
2022-10-31 23:08:06 +0000

    apis added

---
+++ TopicName.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+const topicBody = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true,
+  },
+  delimiter: {
+    type: String,
+    required: true,
+  },
+  cat: {
+    type: String,
+    required: true,
+  }
+})
+
+const TopicSchema = new mongoose.Schema({
+  topicName: {
+    type: String,
+    required: true,
+  },
+  percentage: {
+    type: Number,
+  },
+  topicBody: [{
+    type: topicBody,
+    required: true,
+  }]
+})
+
+const Topic = mongoose.model('TopicName', TopicSchema)
+
+module.exports = Topic
--- server.js
+++ server.js
@@ -2,45 +2,94 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const Topic = require('./TopicName')
 const port = 3000
 const CATEGORIES = ["UNDERSTOOD", "SOMEWHAT UNDERSTOOD", "NOT CLEAR", "WHAT RUBBISH"]
 app.use(bodyParser.json())
 app.use(cors())
-app.post('/text', (req, res) => {
-  console.log(req.body)
-  const { test } = req.body;
-  const split1 = test.split(/(?=[-_\n.,?{}()\[\]""'|:/\\])|(?<=[-_\n.,?{}()\[\]""'|:/\\])/);
-  console.log(split1); // 👉️ ['a', 'b' , 'c', 'd']
-  let text = ''
-  const newObj = []
-  for (let ele = 0; ele < split1.length; ele++) {
-    text += split1[ele];
-    if (ele % 2 == 0) {
-      console.log(split1[ele + 1])
-      const tt = { text: split1[ele], deliminator: split1[ele + 1], cat: CATEGORIES[Math.floor(Math.random() * CATEGORIES.length)] }
-      newObj.push(tt)
+
+
+app.post('/add', async (req, res) => {
+  try {
+    const { topicName, topicBody } = req.body;
+    const split1 = topicBody.split(/(?=[-_\n.,?{}()\[\]""'|:/\\])|(?<=[-_\n.,?{}()\[\]""'|:/\\])/);
+    let text = ''
+    const newObj = []
+    for (let ele = 0; ele < split1.length; ele++) {
+      text += split1[ele];
+      if (ele % 2 == 0) {
+        const tt = { text: split1[ele], delimiter: split1[ele + 1], cat: CATEGORIES[Math.floor(Math.random() * CATEGORIES.length)] }
+        newObj.push(tt)
+      }
     }
+    const topicNameObject = await Topic.create({ topicName, topicBody: newObj });
+    const { topicBody: responseObject } = topicNameObject;
+    console.log(topicNameObject)
+    res.json({ text, categorized: responseObject })
+  } catch (error) {
+    res.json(error)
   }
-  res.json({ text, categorized: newObj })
 })
 
-app.get('/', (req, res) => {
-  const test = `Hello, this is a test. How are \'you\'?
-  hahahaha. [This works perfectly]. doesn\'t it | this
-  is:amazing\\\or is it`
-  // const str = 'a-b_c-d';
-  res.json({ message: "HelloWorld!", secret: process.env.TESTING })
+app.get('/topics', async (req, res) => {
+  try {
+    const topics = await Topic.find({}, { _id: 0 }).lean()
+    res.json({ topics })
+  } catch (error) {
+    res.json(error)
+  }
 })
 
-app.listen(port, () => {
-  // const { Pool } = require("pg")
-
-  // const db = new Pool()
-
-  // db.query(`CREATE TABLE IF NOT EXISTS testing(id SERIAL PRIMARY KEY);`)
-  console.log(`Example app listening on port ${port}`)
-
+app.get('/topic/:topicId', async (req, res) => {
+  try {
+    const { topicId } = req.params;
+    const topicData = await Topic.findById(mongoose.Types.ObjectId(topicId), { __v: 0 })
+    res.json({ topic: topicData })
+  } catch (error) {
+    res.json(error)
+  }
 })
+
+app.put('/block/:blockId', async (req, res) => {
+  try {
+    const { blockId } = req.params;
+    const { category } = req.body;
+    const update = await Topic.updateOne(
+      {
+        "topicBody": { "$elemMatch": { _id: mongoose.Types.ObjectId(blockId) } }
+      },
+      {
+        "$set": { "topicBody.$.cat": category }
+      })
+    update.modifiedCount == 1 ?
+      res.json({ message: 'Successfully updated!' }) :
+      res.json({ message: 'Something went wrong!' })
+
+  } catch (error) {
+    res.json(error)
+  }
+})
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewURLParser: true,
+  useUnifiedTopology: true
+});
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+  app.listen(port, () => {
+    // const { Pool } = require("pg")
+
+    // const db = new Pool()
+
+    // db.query(`CREATE TABLE IF NOT EXISTS testing(id SERIAL PRIMARY KEY);`)
+    console.log(`Example app listening on port ${port}`)
+
+  })
+
+});
 
 // const express = require('express');
 // const promise = require('bluebird');
